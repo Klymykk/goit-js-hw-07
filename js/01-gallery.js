dace6343fc8534264ee7eab49934709afa61e70a
Klymuk <114335106+Klymykk@users.noreply.github.com>
2023-05-02 10:40:41 +0300

    task

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector(".gallery");
+
+const item = galleryItems.map( ({preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  }).join(" ");
+  gallery.insertAdjacentHTML("beforeend", item);
+
+  gallery.addEventListener("click", onClick);
+
+  function onClick(event){
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    console.log(event.currenTarget)
+  }
